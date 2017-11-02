48a3956d68677525f66072d1fd136d1b47ce98ce
kermit.bu <kermit.bu@qq.com>
2017-11-02 16:14:19 +0800

    中文注释crash问题

--- src/inifile.cpp
+++ src/inifile.cpp
@@ -538,29 +538,26 @@ void IniFile::trimright(string &str, char c/*=' '*/)
 void IniFile::trim(string &str)
 {
     //trim head
+	int i = 0;
+	int len = str.length();
+	while((str[i] == ' ' || str[i] == '\t') && str[i] != '\0'){
+		i++;
+	}
+	if(i != 0){
+		str = string(str,i,len-i);
+	}
+		
+	//trim tail
+	len = str.length();
+	for(i = len - 1; i >= 0; --i )
+	{
+		if(str[i] != ' ' && str[i] != '\t')
+		{
+			break;
+		}
+	}
 
-    int len = str.length();
-
-    int i = 0;
-
-    while (isspace(str[i]) && str[i] != '\0') {
-        i++;
-    }
-
-    if (i != 0) {
-        str = string(str, i, len - i);
-    }
-
-    //trim tail
-    len = str.length();
-
-    for (i = len - 1; i >= 0; --i) {
-        if (!isspace(str[i])) {
-            break;
-        }
-    }
-
-    str = string(str, 0, i + 1);
+	str = string(str,0,i+1);
 }
 }
 #endif
